921cf7abda1bc9dbcd71e0c8d905009adc1fe087
ernestoac87 <ernestoac_h@hotmail.com>
2017-10-23 06:55:25 -0500

    version3

--- alumno.entidad.php
+++ alumno.entidad.php
@@ -8,6 +8,7 @@ class Alumno
     private $Sexo;
     private $FechaNacimiento;
 	private $ubicacion;
+	private $fotografia;
 
     public function __GET($k){ return $this->$k; }
     public function __SET($k, $v){ return $this->$k = $v; }
