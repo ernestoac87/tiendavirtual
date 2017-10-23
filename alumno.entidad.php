c99c76b87aeb2c22bdd730fb6ef0cc835d67520e
ernestoac87 <ernestoac_h@hotmail.com>
2017-10-23 06:45:43 -0500

    Version 2

---
+++ alumno.entidad.php
@@ -0,0 +1,16 @@
+<?php
+
+class Alumno
+{
+    private $id;
+    private $Nombre;
+    private $Apellido;
+    private $Sexo;
+    private $FechaNacimiento;
+	private $ubicacion;
+
+    public function __GET($k){ return $this->$k; }
+    public function __SET($k, $v){ return $this->$k = $v; }
+}
+
+?>
